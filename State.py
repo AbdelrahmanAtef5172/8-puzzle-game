e53e80c47b6f22bf76ffe1f9a0fff02235fd2e76
AbdelrahmanAtef5172 <abdelrahmanatef426@gmail.com>
2025-10-24 17:54:31 +0300

    Update State.py

--- State.py
+++ State.py
@@ -1,53 +1,37 @@
 import copy
-class State :
+class State:
 
-    # CLASS CONSTRUCTOR
-    def __init__ (self , tiles):
-        self.tiles = tiles
-
-    # CHECK IF THIS STATE IS GOAL STATE
+    #CLASS CONSTRUCTOR
+    def __init__(self, tiles):
+        self.tiles = tiles 
+    #METHOD TO CHECK THE CURRENT STATE IS GOAL
     def isGoal(self):
-        number_comparing = 0
-        # looping through the 2d array
-        for i in range(3):
-           for j in range(3) :
-               # checking the first tile
-               if i==0 and j==0 :
-                   if(self.tiles[i][j]!=" "):
-                       return False
-               else :
-                   number_comparing+=1
-                   if(self.tiles[i][j]!= number_comparing):
-                       return False
-        return True
-    
-    # Get the positon of the empty tile in 2d array
+        goal = [[0,1,2],[3,4,5],[6,7,8]]
+        return self.tiles == goal
+    #METHOD TO GET THE POSITION OF 0 TILE
     def getEmptyTilePosition(self):
         for i in range(3):
             for j in range(3):
-                if(self.tiles[i][j]==" "):
-                    return i , j
-    
-    # get copy of current state
+                if self.tiles[i][j] == 0:
+                    return i, j
+    #METHOD RETURNS A DEEP COPY OF THE STATE
     def getCopy(self):
-        return copy.deepcopy(self.tiles)    
-
-    # Get the neighbours of the state
+        return State(copy.deepcopy(self.tiles))
+    #METHOD GET THE NEIGHBOURS OF THE CURRENT STATE
     def getNeighbours(self):
-        neighbours=[]
-        i , j = self.getEmptyTilePosition()
-        for row , column in [[i+1,j],[i,j+1],[i-1,j],[i,j-1]]:
-            if ( 3>row>=0 and 3>column>=0):
-                copiedArray = self.getCopy()
-                temp = copiedArray[i][j]
-                copiedArray[i][j]= copiedArray[row][column]
-                copiedArray[row][column] = temp
-                neighbours.append(copiedArray)
+        neighbours = []
+        i, j = self.getEmptyTilePosition()
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]  # Down, Up, Right, Left
+        for di, dj in directions:
+            row, col = i + di, j + dj
+            if 0 <= row < 3 and 0 <= col < 3:
+                new_state = self.getCopy()
+                new_state.tiles[i][j], new_state.tiles[row][col] = new_state.tiles[row][col], new_state.tiles[i][j]
+                neighbours.append(new_state)
         return neighbours
-    
-
-    
-
-                       
-
-
+    #EQUAL OPERATOR
+    def __eq__(self, other):
+        return self.tiles == other.tiles
+    #TO USE SETS
+    def __hash__(self):
+        return hash(str(self.tiles)) 
